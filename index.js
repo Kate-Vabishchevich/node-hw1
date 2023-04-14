bf4d7ddedd4e83bcb4f78816d516d1bfffa75891
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2023-04-14 16:01:55 +0300

    one part

---
+++ index.js
@@ -0,0 +1,39 @@
+// const contacts = require("./db/contacts.json");
+const contacts = require("./contacts");
+
+// const { listContacts, getContactById } = require("./contacts");
+
+// const argv = require("yargs").argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction(argv);
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "qdggE76Jtbfd9eWJHrssH" });
+invokeAction({
+  action: "add",
+  name: "Kate",
+  email: "kate@mail.com",
+  phone: "(000)111222",
+});
